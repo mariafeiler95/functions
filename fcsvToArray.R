382d1389651824f499f7b431d92dc0f9113b736c
Maria Feiler <63727926+mariafeiler95@users.noreply.github.com>
2025-04-21 16:41:32 -0400

    Update fcsvToArray.R

--- fcsvToArray.R
+++ fcsvToArray.R
@@ -1,24 +1,22 @@
-fcsvToArray <- function(file = NULL, dir = NULL, lms = NULL, nams = NULL){
-  if(is.null(file) && !is.null(dir)){
-    file = list.files(path = dir, pattern = "fcsv", full.names = TRUE)
-  }
-  
-  if(is.null(lms)){
-    temp = read.csv(file = file[1], skip = 3, header = FALSE, row.names = 12)
-    lms = rownames(temp)
-  }
-  
-  if(is.null(nams)){nams = list.files(path = dir, pattern = "fcsv")}
-  
-  LM = array(data = NA, 
-             dim = c(length(lms), 3, length(file)),
-             dimnames = list(lms, c("X", "Y", "Z"), file))
-  
-  for(i in 1:length(file)){
-      temp = read.csv(file = file[[i]], skip = 3, header = FALSE, row.names = 12)
-      LM[,,i] = as.matrix(temp[lms,2:4])
-  }
-  
-  dimnames(LM)[[3]] = nams
-  return(LM)
+fcsvToArray <- function(file, dir, lms = NULL, nams = NULL){
+    fs = paste(dir, file, sep = "/")
+
+    if(is.null(lms)){
+        temp1 = read.csv(file = fs[1], skip = 3, header = FALSE, row.names = 12)
+        lms = rownames(temp1)
+    }
+    
+    LM = array(data = NA, 
+               dim = c(length(lms), 3, length(fs)),
+               dimnames = list(lms, c("X", "Y", "Z"), fs))
+    
+    for(i in 1:length(fs)){
+        temp2 = Morpho::read.fcsv(fs[[i]])
+        row.names(temp2) = lms
+        LM[,,i] = as.matrix(temp2[lms,])
+    }
+    
+    if(is.null(nams)){dimnames(LM)[[3]] = fs} else{dimnames(LM)[[3]] = nams}
+    
+    return(LM)
 }
