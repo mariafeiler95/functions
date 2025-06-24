d94f8dfd10d0eac0ca3a51c9f8a75d7158a410ec
Maria Feiler <63727926+mariafeiler95@users.noreply.github.com>
2025-06-24 16:18:22 -0400

    Update mShapeDists.R
    Included ability to get all specimens in order (n = "all") and suppress distance from mean shape and retain only specimen names (nams.only parameter).

--- mShapeDists.R
+++ mShapeDists.R
@@ -4,17 +4,23 @@
 # Given an array of coordinate data and a number of desired specimens, find the 
 # desired number of specimens that are closest to the mean shape. 
 
-# 03/03/3023
+# 06/24/2025
 # M.E.F.
 # mariaefeiler@gmail.com
 
-# R4.1.3, RStudio 2022.7.0.548
+# R4.4.0, RStudio 2024.04.1+748
 # Windows 10 x64
 
-# SOURCE:   Based on geomorph's findMeanSpec()
+# SOURCE: Based on geomorph's findMeanSpec()
 
-# @param A  Array of coordinate data, either 2D or 3D
-# @param n  Number of desired specimens
+# @param A             Array of coordinate data, either 2D or 3D
+# @param n             Number of desired specimens
+#                      If "all", returns all specimens in order.
+#                      If a negative integer, gives the specimens furthest from 
+#                      the mean shape.
+#                      Default value = 3
+# @param nams.only     If TRUE, only returns specimen names. If FALSE, then 
+#                      distances are also reported.
 
 # @return   A vector of Procrustes distances from the calculated mean shape,
 #           where names are the names of the specimens.
@@ -27,7 +33,13 @@
 # Find the two specimens closest to the mean shape.
 # mShapeDists(A = scalgpa$coords, n = 2)
 
-mShapeDists <- function(A, n){
+# Find the two specimens farthest from the mean shape.
+# mShapeDists(A = scalgpa$coords, n = -2)
+
+# Sort the specimens in order from closest to farthest from mean shape, retaining only names.
+# mShapeDists(A = scalgpa$coords, n = "all", nams.only = TRUE)
+
+mShapeDists <- function(A, n = 3, nams.only = FALSE){
   require(geomorph)
   if(!is.array(A)) {
     stop("Data matrix not a 3D array (see 'arrayspecs').") }
@@ -36,5 +48,16 @@ mShapeDists <- function(A, n){
   x <- rbind(x, as.vector(t(ref)))
   dists <- as.matrix(dist(x))[,(nrow(x))]
   dists <- sort(dists[-which(dists == 0)]) #Remove 0 and sort
-  return(dists[1:n])
+
+  if(nams.only == TRUE){
+        if(is.character(n)){return(names(dists))}
+        if(n < 0){return(tail(names(dists), abs(n)))}
+        if(n > 0){return(names(dists)[1:n])}
+    }
+    
+    if(nams.only == FALSE){
+        if(is.character(n)){return(dists)}
+        if(n < 0){return(tail(dists, abs(n)))}
+        if(n > 0){return(dists[1:n])}
+    }
 }
