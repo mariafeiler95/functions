54fa3425de789d690921953b7cb0943d4d584c60
Maria Feiler <63727926+mariafeiler95@users.noreply.github.com>
2025-09-30 15:59:10 -0400

    Create point2plane.R

---
+++ point2plane.R
@@ -0,0 +1,37 @@
+point2plane <- function(p,a,b,c)
+{
+  if (!is.point(p,3) || !is.point(a,3) || !is.point(b,3) || !is.point(c,3))
+  {stop('all arguments must be numeric points of dimension 3')}
+
+  p1<-p[1]; p2<-p[2]; p3<-p[3];
+  a1<-a[1]; a2<-a[2]; a3<-a[3];
+  b1<-b[1]; b2<-b[2]; b3<-b[3];
+  c1<-c[1]; c2<-c[2]; c3<-c[3];
+
+  Cx <-(-a2*b3+a2*c3+a3*b2-a3*c2-b2*c3+b3*c2)/(a1*b2-a1*c2-a2*b1+a2*c1+b1*c2-b2*c1);
+  Cy <-(a1*b3-a1*c3-a3*b1+a3*c1+b1*c3-b3*c1)/(a1*b2-a1*c2-a2*b1+a2*c1+b1*c2-b2*c1);
+  Cz <--1;
+  if (abs(Cx)==Inf || abs(Cy)==Inf)
+    #this part is for when the plane is vertical so Cx or Cy are Inf or -Inf
+  {ra<-range(a); a<-a+(ra[2]-ra[1])*runif(3,0,10^(-10))
+  rb<-range(b); b<-b+(rb[2]-rb[1])*runif(3,0,10^(-10))
+  rc<-range(c); c<-c+(rc[2]-rc[1])*runif(3,0,10^(-10))
+  a1<-a[1]; a2<-a[2]; a3<-a[3];
+  b1<-b[1]; b2<-b[2]; b3<-b[3];
+  c1<-c[1]; c2<-c[2]; c3<-c[3];
+  Cx <-(-a2*b3+a2*c3+a3*b2-a3*c2-b2*c3+b3*c2)/(a1*b2-a1*c2-a2*b1+a2*c1+b1*c2-b2*c1);
+  Cy <-(a1*b3-a1*c3-a3*b1+a3*c1+b1*c3-b3*c1)/(a1*b2-a1*c2-a2*b1+a2*c1+b1*c2-b2*c1);
+  }
+  C0 <-(a1*b2*c3-a1*b3*c2-a2*b1*c3+a2*b3*c1+a3*b1*c2-a3*b2*c1)/(a1*b2-a1*c2-a2*b1+a2*c1+b1*c2-b2*c1);
+
+  num.dis<-Cx*p1+Cy*p2+Cz*p3+C0; #numerator of the distance equation
+
+  t<-(Cx*a1-Cx*p1+Cy*a2-Cy*p2+Cz*a3-Cz*p3)/(Cx^2+Cy^2+Cz^2)
+  prj<-p+t*c(Cx,Cy,Cz)
+
+  dis<-num.dis/sqrt(Cx^2+Cy^2+Cz^2);
+
+  list(distance=dis, #distance
+       prj.pt2plane=prj #c(pr.x,pr.y,pr.z) #point of orthogonal projection (i.e. closest point) on the plane
+  )
+}
